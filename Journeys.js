b2d471e199f298c36930d925ebbedb5c82d5a626
Mengxing-LI <60501376+Mengxing-LI@users.noreply.github.com>
2023-01-12 10:51:13 +1100

    Update Journeys.js

--- Journeys.js
+++ Journeys.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/mengxing/ckwx4c76c8m5x14o5jiixb9yy', //mapbox://styles/examples/cj68bstx01a3r2rndlud0pwpv
+    style: 'mapbox://styles/examples/cj68bstx01a3r2rndlud0pwpv', //mapbox://styles/mengxing/ckwx4c76c8m5x14o5jiixb9yy
     accessToken: 'pk.eyJ1IjoibWJ4c29sdXRpb25zIiwiYSI6ImNrMm01aG9hdTBlZGwzbXQ1ZXVrNHNmejAifQ.QHQA0N6XPWddCXtvoODHZg',
     showMarkers: true,
     theme: 'light',
